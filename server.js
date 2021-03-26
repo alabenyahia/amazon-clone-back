a89ed67169c1a54ae814bb5a7b429d3edda692b4
Ala Ben Yahia <alabny18@gmail.com>
2021-03-27 00:43:25 +0100

    Add default server boilerplate

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-const express = require('express')
-require('dotenv').config()
-require('./database/connection')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+require("./database/connection");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port", process.env.PORT);
+});
