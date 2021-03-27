f5f30fe3becd63a7cb3f69819cbe6481d7555f42
Ala Ben Yahia <alabny18@gmail.com>
2021-03-27 21:27:46 +0100

    Implement user registration api functionality

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
 const userModel = require("../../database/model/User");
+const { registerValidationSchema } = require("../../validation/auth");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res) => {
+    const { error } = registerValidationSchema.validate(req.body, { abortEarly: false });
+
+    if (error) {
+        let errObj = {};
+        error.details.forEach((er) => (errObj = { ...errObj, [er.context.label]: er.message }));
+        return res.status(400).json({ registerValidationError: errObj });
+    }
+
+    // check if email exists
+    const emailIsNotUnique = await userModel.findOne({ email: req.body.email });
+    if (emailIsNotUnique)
+        return res.status(400).json({ registerValidationError: { email: "Email already exists" } });
+
+    const pwdSalt = await bcrypt.genSalt(10);
+    const hashedPwd = await bcrypt.hash(req.body.password, pwdSalt);
+
     try {
-        const user = await userModel.create({ ...req.body });
+        const user = await userModel.create({ ...req.body, password: hashedPwd });
         return res.status(200).json({ user });
     } catch (error) {
         return res.status(400).json({ error });
